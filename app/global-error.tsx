ff38e2dc1d42b915c744f016a86781d0047c700d
Hiranmoy75 <hiranmoymandalucb@gmail.com>
2025-12-11 00:44:58 +0530

    Add global-error.tsx with edge runtime to fix default route issues

---
+++ app/global-error.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+export const runtime = 'edge';
+
+export default function GlobalError({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    return (
+        <html>
+            <body>
+                <div className="flex flex-col items-center justify-center min-h-screen py-2">
+                    <h2>Something went wrong!</h2>
+                    <button onClick={() => reset()}>Try again</button>
+                </div>
+            </body>
+        </html>
+    )
+}
