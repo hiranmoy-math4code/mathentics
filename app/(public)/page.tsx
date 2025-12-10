660ca094c1355eded58112faf8db2b6f6fc8b15f
Hiranmoy75 <hiranmoymandalucb@gmail.com>
2025-12-11 00:24:30 +0530

    Add Edge Runtime to public page to resolve _not-found ambiguity

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+export const runtime = 'edge';
 import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import {
