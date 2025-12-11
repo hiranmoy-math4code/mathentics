dc557b7e1cc13d60fc514fd7da12caf8ef66514d
Hiranmoy75 <hiranmoymandalucb@gmail.com>
2025-12-11 09:59:10 +0530

    Add edge runtime to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { updateSession } from "@/lib/supabase/middleware"
 import type { NextRequest } from "next/server"
-
+export const runtime = 'edge'
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
